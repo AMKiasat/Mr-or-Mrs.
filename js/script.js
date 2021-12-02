91452b3e232df279c78fc5a7c961bc1ad752721e
AMKiasat <kiasatamirm@gmail.com>
2021-12-02 23:17:57 +0330

    Bug fix

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 // This function does the work of submit button and shows the results of the search in localStorage and https://api.genderize.io/?name= web site
 function submitN() {
     let name = document.getElementById("name").value;
+    if (!checkName(name)) {
+        document.getElementById("problem").innerHTML = "This can not be a name.";
+        return
+    }
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open("GET", "https://api.genderize.io/?name=" + name, false);
     xmlHttp.send(null);
@@ -9,6 +13,12 @@ function submitN() {
         document.getElementById("maleOrFemale").innerHTML = "submit a name.";
         document.getElementById("probability").innerHTML = "submit a name.";
         document.getElementById("saved").innerHTML = "submit a name.";
+        if (name == "")
+            document.getElementById("problem").innerHTML = "Enter a name.";
+        else if (json.probability == 0)
+            document.getElementById("problem").innerHTML = "There isn't any name like  yours.";
+        if (localStorage.getItem(name) != null)
+            document.getElementById("saved").innerHTML = localStorage.getItem(name)
     }
     else {
         document.getElementById("maleOrFemale").innerHTML = json.gender;
@@ -17,12 +27,18 @@ function submitN() {
             document.getElementById("saved").innerHTML = localStorage.getItem(name)
         else
             document.getElementById("saved").innerHTML = "submit a name.";
+
+        document.getElementById("problem").innerHTML = "";
     }
 }
 
 // This function does the work of the save button and it saves the male or female results in localStorage
 function saveN() {
     let name = document.getElementById("name").value;
+    if (!checkName(name)) {
+        document.getElementById("problem").innerHTML = "This can not be a name.";
+        return
+    }
     if (name != "") {
         if (document.getElementById("male").checked) {
             if (localStorage.getItem(name) != null)
@@ -42,24 +58,38 @@ function saveN() {
             localStorage.setItem(name, document.getElementById("maleOrFemale").innerHTML);
             document.getElementById("saved").innerHTML = document.getElementById("maleOrFemale").innerHTML;
         }
+        document.getElementById("problem").innerHTML = "";
     }
     else {
         document.getElementById("maleOrFemale").innerHTML = "submit a name.";
         document.getElementById("probability").innerHTML = "submit a name.";
         document.getElementById("saved").innerHTML = "submit a name.";
+        document.getElementById("problem").innerHTML = "Enter a name.";
     }
 }
 
 // This function does the work of the clear button and it removes the entered name from localStorage
 function clearN() {
     let name = document.getElementById("name").value;
+    if (!checkName(name)) {
+        document.getElementById("problem").innerHTML = "This can not be a name.";
+        return
+    }
     if (name != "") {
         localStorage.removeItem(name);
         document.getElementById("saved").innerHTML = "submit a name.";
+        document.getElementById("problem").innerHTML = "";
     }
     else {
         document.getElementById("maleOrFemale").innerHTML = "submit a name.";
         document.getElementById("probability").innerHTML = "submit a name.";
         document.getElementById("saved").innerHTML = "submit a name.";
+        document.getElementById("problem").innerHTML = "Enter a name.";
     }
+}
+
+// check if the name is a name or not
+function checkName(name) {
+    var regex = /^[a-z A-Z]+$/;
+    return regex.test(name);
 }
